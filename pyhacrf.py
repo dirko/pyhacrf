7352202ba2b74e8ee03b664f22130343822fd278
Dirko Coetsee <dirko@praelexis.com>
2015-02-26 13:17:53 +0200

    Started putting up skeleton.

---
+++ pyhacrf.py
@@ -0,0 +1,16 @@
+# Authors: Dirko Coetsee
+# License: 3-clause BSD
+
+""" Implements a Hidden Alignment Conditional Random Field (HACRF). """
+
+
+class Hacrf(object):
+    """ Hidden Alignment Conditional Random Field with L2 regularizer.
+
+    See *A Conditional Random Field for Discriminatively-trained Finite-state String Edit Distance* by McCallum, Bellare, and Pereira,
+        and the report *Conditional Random Fields for Noisy text normalisation* by Dirko Coetsee.
+    """
+
+    def __init__(self):
+        pass
+
---
+++ tests/test_model.py
@@ -0,0 +1,15 @@
+""" Tests for the model. """
+
+import unittest
+
+from numpy.testing import assert_array_almost_equal
+import numpy as np
+
+
+class TestHacrf(unittest.TestCase):
+    def test_marginalize_small_edge(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
