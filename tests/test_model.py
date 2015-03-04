4def9175690d654b9e581013e7666175362d9f2f
Dirko Coetsee <dirko@ml.sun.ac.za>
2015-03-04 20:40:38 +0200

    Finished lattice test.

--- tests/test_model.py
+++ tests/test_model.py
@@ -29,7 +29,7 @@ class TestModel(unittest.TestCase):
         transitions = [(0, 0, (1, 1)),
                        (0, 1, (0, 1)),
                        (0, 0, (1, 0)),
-                       (0, 3, lambda i, j, k: 0, 2)]
+                       (0, 3, lambda i, j, k: (0, 2))]
         state_machine = (states, transitions)
         x = np.zeros((2, 3, 9))
         #
@@ -38,17 +38,38 @@ class TestModel(unittest.TestCase):
         # 0.  .  .
         #  0  1  2
         actual_lattice = _Model._build_lattice(x, state_machine)
-        expected_lattice = [(0, 0, 0),
+        expected_lattice = [
+                            (0, 0, 0),
                             (0, 0, 1),
                             (0, 0, 3),
                             (0, 0, 1, 1, 0, 0),
                             (0, 0, 0, 1, 0, 1),
                             (0, 0, 1, 0, 0, 0),
-                            (0, 0, 0),
-                            (0, 0, 1),
-                            (0, 0, 3),
-
+                            (0, 0, 0, 2, 0, 3),
+                            (0, 1, 0),
+                            (0, 1, 1),
+                            (0, 1, 3),
+                            (0, 1, 1, 2, 0, 0),
+                            (0, 1, 0, 2, 0, 1),
+                            (0, 1, 1, 1, 0, 0),
+                            (0, 2, 0),
+                            (0, 2, 1),
+                            (0, 2, 3),
+                            (0, 2, 1, 2, 0, 0),
+                            (1, 0, 0),
+                            (1, 0, 1),
+                            (1, 0, 3),
+                            (1, 0, 1, 1, 0, 1),
+                            (1, 0, 1, 2, 0, 3),
+                            (1, 1, 0),
+                            (1, 1, 1),
+                            (1, 1, 3),
+                            (1, 1, 1, 2, 0, 1),
+                            (1, 2, 0),
+                            (1, 2, 1),
+                            (1, 2, 3),
                             ]
+        self.assertEqual(actual_lattice, expected_lattice)
 
 if __name__ == '__main__':
     unittest.main()
