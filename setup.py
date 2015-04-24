7d7f3e944585e8565d9c4c6b4f24442057b8d5c8
Dirko Coetsee <dirko@ml.sun.ac.za>
2015-04-24 21:31:39 +0200

    Removed some classifiers.

--- setup.py
+++ setup.py
@@ -10,19 +10,17 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pyhacrf',
-    version='0.0.5',
+    version='0.0.6',
     packages=['pyhacrf'],
     install_requires=['numpy>=1.9', 'PyLBFGS>=0.1.3'],
     url='https://github.com/dirko/pyhacrf',
-    download_url='https://github.com/dirko/pyhacrf/tarball/0.0.5',
+    download_url='https://github.com/dirko/pyhacrf/tarball/0.0.6',
     license='BSD',
     author='Dirko Coetsee',
     author_email='dpcoetsee@gmail.com',
     description='Hidden alignment conditional random field, a discriminative string edit distance',
     long_description=long_description,
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Console',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
