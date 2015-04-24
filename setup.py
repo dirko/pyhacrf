6e18513d5138d0115410e1bd20ea4dcf458abcc6
Dirko Coetsee <dirko@ml.sun.ac.za>
2015-04-24 20:41:20 +0200

    Added RST readme file.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='pyhacrf',
-    version='0.0.3',
+    version='0.0.4',
     packages=['pyhacrf'],
     install_requires=['numpy>=1.9', 'PyLBFGS>=0.1.3'],
     url='https://github.com/dirko/pyhacrf',
-    download_url='https://github.com/dirko/pyhacrf/tarball/0.0.3',
+    download_url='https://github.com/dirko/pyhacrf/tarball/0.0.4',
     license='BSD',
     author='Dirko Coetsee',
     author_email='dpcoetsee@gmail.com',
