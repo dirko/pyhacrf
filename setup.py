06714887169e8f797d9fb6e1de8a6802a49daa17
Dirko Coetsee <dirko@ml.sun.ac.za>
2015-08-04 20:38:19 +0200

    Release newest refactorings from fgregg.

--- setup.py
+++ setup.py
@@ -24,13 +24,13 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pyhacrf',
-    version='0.0.10',
+    version='0.0.11',
     packages=['pyhacrf'],
     install_requires=['numpy>=1.9', 'PyLBFGS>=0.1.3'],
     ext_modules=[NumpyExtension('pyhacrf.algorithms', 
                                 ['pyhacrf/algorithms.c'])],
     url='https://github.com/dirko/pyhacrf',
-    download_url='https://github.com/dirko/pyhacrf/tarball/0.0.10',
+    download_url='https://github.com/dirko/pyhacrf/tarball/0.0.11',
     license='BSD',
     author='Dirko Coetsee',
     author_email='dpcoetsee@gmail.com',
